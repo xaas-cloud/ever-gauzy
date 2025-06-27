4d962928cce17e41a5fbf8197e2553446df776d6
rolandm99 <manfulmweze99@gmail.com>
2025-06-27 14:42:34 +0200

    feat: add heler functions for authentication and APIs configuration

--- apps/gauzy-mcp-server/src/common/version.ts
+++ apps/gauzy-mcp-server/src/common/version.ts
@@ -1,5 +1,10 @@
 import * as pkg from '../../package.json';
 
+interface PackageJson {
+  version: string;
+}
+
+
 export function getVersion() {
-  return (pkg as any).version;
+  return (pkg as PackageJson).version;
 }
