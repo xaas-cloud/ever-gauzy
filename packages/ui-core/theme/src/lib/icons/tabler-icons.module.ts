20c334bbe5b865fca01e3766510b621d4e8d3aef
Rishiraj S <131572506+rishi-rj-s@users.noreply.github.com>
2026-01-28 10:55:54 +0530

    refactor(icons): modernize dependency injection using inject()

--- packages/ui-core/theme/src/lib/icons/tabler-icons.module.ts
+++ packages/ui-core/theme/src/lib/icons/tabler-icons.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
+import { inject, NgModule } from '@angular/core';
 import { NbIconLibraries } from '@nebular/theme';
 import { evaToTablerIcons } from './eva-to-tabler-icons.map';
 
 @NgModule()
 export class NbTablerIconsModule {
-	constructor(private iconLibraries: NbIconLibraries) {
+	private readonly _iconLibraries = inject(NbIconLibraries);
+
+	constructor() {
 		this.registerTablerPack();
 	}
 
 	registerTablerPack() {
 		// We register this pack as 'eva' to maintain backward compatibility with existing usages of the Eva icon pack in templates and configuration.
-		this.iconLibraries.registerSvgPack('eva', evaToTablerIcons);
-		this.iconLibraries.setDefaultPack('eva');
+		this._iconLibraries.registerSvgPack('eva', evaToTablerIcons);
+		this._iconLibraries.setDefaultPack('eva');
 	}
 }
